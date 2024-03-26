704ad689515bd6d591f4cecf32713922192dc142
Sehreen22Khan <ryanabbas1234@gmail.com>
2024-03-25 21:26:28 -0700

    optimized

--- rpa_v2.py
+++ rpa_v2.py
@@ -2,15 +2,16 @@ import threading
 from tkinter import messagebox
 import tkinter
 import traceback
+import numpy as np
 import pyautogui
 from PIL import ImageDraw
 import time
 import pyttsx3
  
-green_band = (174, 255, 50)
-green_bg = (161, 187, 137)
-purple_bg = (166, 160, 180)
-purple_band = (150, 4, 212)
+green_band = [174, 255, 50]
+green_bg = [161, 187, 137]
+purple_bg = [166, 160, 180]
+purple_band = [150, 4, 212]
 
 def show_popup(message):
     # Create a Tkinter root window
@@ -28,7 +29,7 @@ def speak_thread(text):
     engine.say(text)
     engine.runAndWait()
  
-def draw_red_circle(image, x, y, radius=10):
+def draw_circle(image, x, y, radius=10):
     draw = ImageDraw.Draw(image)
     x1 = x - radius
     y1 = y - radius
@@ -36,16 +37,12 @@ def draw_red_circle(image, x, y, radius=10):
     y2 = y + radius
     draw.ellipse((x1, y1, x2, y2), outline='red')
     # image.show()
- 
-def get_top_right_coordinates(screenshot, color=(174, 255, 50)):
-    screenshot_width, screenshot_height = screenshot.size
-    for x in range(screenshot_width - 1, -1, -1):
-        for y in range(screenshot_height):
-            pixel_color = screenshot.getpixel((x, y))
-            if pixel_color == color:
-                draw_red_circle(screenshot, x, y)
-                return [x, y]
-    return None
+
+def get_top_right_x(screenshot_array, color):
+    mask = np.all(screenshot_array == color, axis=-1)
+    indices = np.argwhere(mask)
+    max_index = np.argmax(indices[:, 1])
+    return indices[max_index][1]
 
 def get_coordinates():
     point = pyautogui.position()
@@ -74,27 +71,23 @@ try:
     current_status = None
     previous_status = None
     while True:
-        screenshot = pyautogui.screenshot(region=[X_MARGIN_LEFT, Y_TOP_MARGIN, X_MARGIN_RIGHT, Y_BOTTOM_MARGIN])
-        green_band_coordinates = get_top_right_coordinates(screenshot, green_band)
-        green_bg_coordinates = get_top_right_coordinates(screenshot, green_bg)
-        purple_band_coordinates = get_top_right_coordinates(screenshot, purple_band)
-        purple_bg_coordinates = get_top_right_coordinates(screenshot, purple_bg)
-        green_band_x, green_band_y = green_band_coordinates
-        green_bg_x, green_bg_y = green_bg_coordinates
-        purple_band_x, purple_band_y = purple_band_coordinates
-        purple_bg_x, purple_bg_y = purple_bg_coordinates
+        screenshot = np.array(pyautogui.screenshot(region=[X_MARGIN_LEFT, Y_TOP_MARGIN, X_MARGIN_RIGHT, Y_BOTTOM_MARGIN]))
+        green_band_x = get_top_right_x(screenshot, green_band)
+        green_bg_x = get_top_right_x(screenshot, green_bg)
+        purple_band_x = get_top_right_x(screenshot, purple_band)
+        purple_bg_x = get_top_right_x(screenshot, purple_bg)
         if green_band_x < purple_band_x and green_bg_x < purple_bg_x:
             current_status = 'red'
             if current_status != previous_status and previous_status != None:
-                speak('Sell')
-                # sell()
+                # speak('Sell')
+                sell()
                 break
             previous_status = current_status
         elif green_band_x > purple_band_x and green_bg_x > purple_bg_x:
             current_status = 'green'
             if current_status != previous_status and previous_status != None:
-                speak('Buy')
-                # buy()
+                # speak('Buy')
+                buy()
                 break
             previous_status = current_status
         else:
