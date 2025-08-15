fc4f79c73655bf161a91d443dfad092782d4a2a6
Your Name <ryanabbas1234@gmail.com>
2025-08-14 20:51:18 -0700

    for macos and windows

--- move_vid.py
+++ move_vid.py
@@ -1,14 +1,35 @@
 import os
+import platform
 
-os.system("""
-osascript -e '
-tell application "System Events"
-    tell application process "QuickTime Player"
-        set frontmost to true
-        tell window 1
-            set position to {0, 0}
-            set size to {1300, 1300}
+if platform.system() == 'Darwin':  # macOS
+    os.system("""
+    osascript -e '
+    tell application "System Events"
+        tell application process "QuickTime Player"
+            set frontmost to true
+            tell window 1
+                set position to {0, 0}
+                set size to {1300, 1300}
+            end tell
         end tell
-    end tell
-end tell'
-""")
+    end tell'
+    """)
+elif platform.system() == 'Windows':
+    try:
+        import pygetwindow as gw
+
+        windows = gw.getWindowsWithTitle('Media Player')
+
+        if windows:
+            win = windows[0]
+            win.moveTo(0, 0)
+            win.resizeTo(1600, 1000)
+            print("Media Player window repositioned.")
+        else:
+            print("Media Player window not found.")
+    except ImportError:
+        print("pygetwindow not installed. Run 'pip install pygetwindow'")
+    except Exception as e:
+        print(f"Error manipulating window: {e}")
+else:
+    print("Unsupported OS.")
